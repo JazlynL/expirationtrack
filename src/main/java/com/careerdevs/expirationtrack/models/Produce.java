af9513eb2a6a2a717528553531e55f10a357d02e
“JazlynL” <lubenjazlyn35@gmail.com>
2022-07-01 21:35:41 -0400

    creating class for produce

--- src/main/java/com/careerdevs/expirationtrack/models/Produce.java
+++ src/main/java/com/careerdevs/expirationtrack/models/Produce.java
@@ -1,2 +1,11 @@
-package com.careerdevs.expirationtrack.models;public class Produce {
+package com.careerdevs.expirationtrack.models;
+
+
+import javax.persistence.Entity;
+
+// category system
+// use name field
+// use Date class for expiration date
+@Entity
+public class Produce {
 }
