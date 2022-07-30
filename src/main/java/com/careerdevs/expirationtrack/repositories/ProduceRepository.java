23a1f2dca5ecc8225cb4f9d50914863180e758e4
“JazlynL” <lubenjazlyn35@gmail.com>
2022-07-29 22:00:25 -0400

    Updated tracker for the request

--- src/main/java/com/careerdevs/expirationtrack/repositories/ProduceRepository.java
+++ src/main/java/com/careerdevs/expirationtrack/repositories/ProduceRepository.java
@@ -2,15 +2,19 @@ package com.careerdevs.expirationtrack.repositories;
 
 import com.careerdevs.expirationtrack.models.Produce;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.List;
 @Transactional
 public interface ProduceRepository extends JpaRepository<Produce,Long>{
     List<Produce> findAllByTracker_id(long id);
+
     List<Produce> findByName(String name);
     List<Produce> findByQuantity(String quantity);
     List<Produce> findByType(String type);
+
     List<Produce> findByExpirationDate(Long expirationDate);
 
 
