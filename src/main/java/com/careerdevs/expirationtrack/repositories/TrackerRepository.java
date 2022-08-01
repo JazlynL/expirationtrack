9a346dd06268b5b448bd744217c646d93c256aa2
“JazlynL” <lubenjazlyn35@gmail.com>
2022-07-31 23:24:08 -0400

    Updated tracker for the request

--- src/main/java/com/careerdevs/expirationtrack/repositories/TrackerRepository.java
+++ src/main/java/com/careerdevs/expirationtrack/repositories/TrackerRepository.java
@@ -4,10 +4,17 @@ import com.careerdevs.expirationtrack.models.Tracker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TrackerRepository extends JpaRepository<Tracker,Long> {
     
     List <Tracker> findAllByName(String name);
+    List<Tracker> findByName (String name);
+    List<Tracker> findByEmail(String email);
+    List<Tracker> findByAge(Long age);
+
+
+    Optional<Tracker> findById(Long id);
 
 
 }
